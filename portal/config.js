1b2a3f5f9ae426ac12c6fa5e93f6cf761e281611
Formiobot <formiobot@example.com>
2019-08-28 15:27:12 +0000

    6.7.1

--- portal/config.js
+++ portal/config.js
@@ -80,7 +80,7 @@ if (Formio) {
   }
 }
 angular.module('formioApp').constant('AppConfig', {
-  appVersion: '6.6.3',
+  appVersion: '6.6.5',
   copyrightYear: (new Date()).getFullYear().toString(),
   sso: sso,
   ssoLogout: ssoLogout,
