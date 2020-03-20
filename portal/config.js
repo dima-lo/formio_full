c7bbc1803a46ec79566ef57ed0e99dd00c881069
Formiobot <formiobot@example.com>
2020-03-20 22:37:02 +0000

    6.9.17

--- portal/config.js
+++ portal/config.js
@@ -89,7 +89,7 @@ if (Formio) {
   }
 }
 angular.module('formioApp').constant('AppConfig', {
-  appVersion: '7.0.0-rc.76',
+  appVersion: '7.0.0-rc.77',
   copyrightYear: (new Date()).getFullYear().toString(),
   sso: sso,
   ssoTeamsEnabled: ssoTeamsEnabled,
